c16709a0353bee77a7fe74b6528bec96244c0e1a
fengyu_xu <fengyu-1995@outlook.com>
2019-09-27 10:00:21 +0800

    新增路由

--- ts_react/src/router/index.ts
+++ ts_react/src/router/index.ts
@@ -1,9 +1,14 @@
 import home from '@/view/home'
+import search from '@/view/search'
 
 export default [
     {
         path: '/',
         exact: true,
         component: home
+    }, {
+        path: '/search',
+        exact: true,
+        component: search
     }
 ]
